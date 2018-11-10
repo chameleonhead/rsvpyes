e79da1a123011787d5c2099a494fa32c2d7031ba
Yuto Nagano <chamelion@hotmail.co.jp>
2018-11-10 21:29:09 +0900

    Domain プロジェクトを作成

---
+++ src/RsvpYes.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RsvpYes.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/RsvpYes.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RsvpYes.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

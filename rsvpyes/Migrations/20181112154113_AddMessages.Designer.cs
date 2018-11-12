46b461f2b1592de72c7adf9d1cdcdd8347ed353c
Yuto Nagano <chamelion@hotmail.co.jp>
2018-11-13 06:37:09 +0900

    使えないけどマイグレーションの履歴を追加

--- rsvpyes/Migrations/20181112154113_AddMessages.Designer.cs
+++ rsvpyes/Migrations/20181112154113_AddMessages.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace rsvpyes.Migrations
 {
     [DbContext(typeof(RsvpContext))]
-    partial class RsvpContextModelSnapshot : ModelSnapshot
+    [Migration("20181112154113_AddMessages")]
+    partial class AddMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ rsvpyes/Migrations/20181112154113_AddMessages.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace rsvpyes.Migrations
+{
+    public partial class AddMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Organization",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "MessageId",
+                table: "RsvpRequests",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Fee",
+                table: "Meetings",
+                nullable: true,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Body = table.Column<string>(nullable: true),
+                    SendTimestamp = table.Column<DateTime>(nullable: false),
+                    SenderUserId = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropColumn(
+                name: "Organization",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "MessageId",
+                table: "RsvpRequests");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Fee",
+                table: "Meetings",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}

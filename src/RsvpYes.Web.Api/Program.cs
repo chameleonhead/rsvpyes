3f2af593dff0b3f5623ccf58e5b9130a5b242fcf
Yuto Nagano <chamelion@hotmail.co.jp>
2019-09-04 00:00:20 +0900

    空の Web プロジェクトを API のプロジェクトに変更

--- src/RsvpYes.Web.Api/Program.cs
+++ src/RsvpYes.Web.Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace RsvpYes.Web
+namespace RsvpYes.Web.Api
 {
     public class Program
     {

90df519e9128118a38c91e59ea8c8f076b737973
Yuto Nagano <chamelion@hotmail.co.jp>
2019-10-19 19:21:13 +0900

    ユーザーのメンテナンスを組織単位でできるように修正

--- rsvpyes/Controllers/UsersController.cs
+++ rsvpyes/Controllers/UsersController.cs
@@ -18,16 +18,28 @@ namespace rsvpyes.Controllers
         }
 
         // GET: Users
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index([FromQuery]string organization)
         {
-            return View((await dataService.Where(u => true))
+            var allUsers = (await dataService.Where(u => true))
                 .OrderBy(e => e.Organization)
-                .ThenBy(e => e.Name));
+                .ThenBy(e => e.Name);
+            var users = allUsers
+                .Where(u => string.IsNullOrEmpty(organization) || u.Organization == organization)
+                .ToList();
+            var organizations = allUsers
+                .Where(u => !string.IsNullOrEmpty(u.Organization))
+                .Select(u => u.Organization)
+                .Distinct()
+                .ToList();
+            ViewData["Organization"] = organization;
+            ViewData["Organizations"] = organizations;
+            return View(users);
         }
 
         // GET: Users/Details/5
-        public async Task<IActionResult> Details(Guid? id)
+        public async Task<IActionResult> Details(Guid? id, [FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             if (id == null)
             {
                 return NotFound();
@@ -43,28 +55,31 @@ namespace rsvpyes.Controllers
         }
 
         // GET: Users/Create
-        public IActionResult Create()
+        public IActionResult Create([FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             return View();
         }
 
         // POST: Users/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([FromForm] User user)
+        public async Task<IActionResult> Create([FromForm] User user, [FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             if (ModelState.IsValid)
             {
                 user.Id = Guid.NewGuid();
                 await dataService.Insert(user);
-                return RedirectToAction(nameof(Index));
+                return RedirectToIndex(organization);
             }
             return View(user);
         }
 
         // GET: Users/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
+        public async Task<IActionResult> Edit(Guid? id, [FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             if (id == null)
             {
                 return NotFound();
@@ -81,8 +96,9 @@ namespace rsvpyes.Controllers
         // POST: Users/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [FromForm] User user)
+        public async Task<IActionResult> Edit(Guid id, [FromQuery]string organization, [FromForm] User user)
         {
+            ViewData["Organization"] = organization;
             if (id != user.Id)
             {
                 return NotFound();
@@ -105,14 +121,15 @@ namespace rsvpyes.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return RedirectToIndex(organization);
             }
             return View(user);
         }
 
         // GET: Users/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
+        public async Task<IActionResult> Delete(Guid? id, [FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             if (id == null)
             {
                 return NotFound();
@@ -130,16 +147,29 @@ namespace rsvpyes.Controllers
         // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> DeleteConfirmed(Guid id, [FromQuery]string organization)
         {
+            ViewData["Organization"] = organization;
             var user = await dataService.Find(id);
             await dataService.Remove(user);
-            return RedirectToAction(nameof(Index));
+            return RedirectToIndex(organization);
         }
 
         private async Task<bool> UserExists(Guid id)
         {
             return (await dataService.Where(e => e.Id == id)).Any();
+        }
+
+        private IActionResult RedirectToIndex(string organization)
+        {
+            if (string.IsNullOrEmpty(organization))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction(nameof(Index), new { organization });
+            }
         }
     }
 }
